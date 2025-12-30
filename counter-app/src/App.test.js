f790ff3761e5aaaa44c372169beb36029a64695d
Khyati <124263941+khyati-2025@users.noreply.github.com>
2025-12-30 12:52:45 +0530

    Fix import error in App.test.js
    Removed an import statement that would cause an error.

--- counter-app/src/App.test.js
+++ counter-app/src/App.test.js
@@ -1,7 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+import { timepass } from './DoesNotExists;
+
+"""The above statement will throw error so, remove it"""
+
 test('renders learn react link', () => {
+  var money = 0;
+  
+  
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
